208eb9f23e4aefedc644e4213936907b38584f95
Hui Song <hui.song@sintef.no>
2020-08-18 14:45:41 +0200

    new bootstrap script for Raspberry Pi

--- edge/bootstrap/setup.sh
+++ edge/bootstrap/setup.sh
@@ -7,11 +7,11 @@ sudo cp ./microsoft-prod.list /etc/apt/sources.list.d/
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo cp ./microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo apt-get update
-sudo apt-get install moby-engine
-sudo apt-get install moby-cli
+sudo apt-get install -y moby-engine
+sudo apt-get install -y moby-cli
 
 sudo apt-get update
-sudo apt-get install iotedge
+sudo apt-get install -y iotedge
 
 
 connection=$(<connection.credential)
