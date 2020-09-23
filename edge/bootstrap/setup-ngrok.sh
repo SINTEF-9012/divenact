7dcb99a296356c896eee6ae16cc351164660fbab
songhui <hui.song@sintef.no>
2020-09-23 23:33:22 +0200

    write token to root...

--- edge/bootstrap/setup-ngrok.sh
+++ edge/bootstrap/setup-ngrok.sh
@@ -3,7 +3,7 @@
 curl -O https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
 unzip ngrok-stable-linux-arm.zip
 sudo cp ./ngrok /opt/
-/opt/ngrok authtoken $(<ngrok.credential)
+sudo /opt/ngrok authtoken $(<ngrok.credential)
 
 sudo cp ./ngrokssh.service /etc/systemd/system/ngrokssh.service
 sudo sudo chmod 644 /etc/systemd/system/ngrokssh.service
