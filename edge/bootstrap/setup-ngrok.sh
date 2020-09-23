f082d0e6aeac8778350da102d053375c23f75d40
songhui <hui.song@sintef.no>
2020-09-23 22:10:27 +0200

    ngrok credetial

--- edge/bootstrap/setup-ngrok.sh
+++ edge/bootstrap/setup-ngrok.sh
@@ -3,4 +3,4 @@
 curl -O https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
 unzip ngrok-stable-linux-arm.zip
 cp ./ngrok /opt/
-/opt/ngrok authtoken $(cat ngrok.credential)
+/opt/ngrok authtoken $(<ngrok.credential)
